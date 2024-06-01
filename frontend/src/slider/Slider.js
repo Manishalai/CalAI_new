49d7707aeb204c572dce28e3e00d3c271270dcdf
singhAman05 <amanshankarsingh05@gmail.com>
2024-06-01 16:57:37 +0530

    slider incoming

---
+++ frontend/src/slider/Slider.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+const Slider = () => {
+  return (
+    <>
+      <h2 class="px-4 text-[36px] font-bold text-[#074D8D] md:text-[20px]">
+        AI Professionals Empowered by CalAI certifications, worked at
+      </h2>
+      <div id="slider" class="flex w-full whitespace-nowrap overflow-x-hidden ">
+        <div
+          id="slide-images"
+          class="flex flex-row h-[80px] text-nowrap animate-infinite-scroll mb-4"
+        />
+        <img src="hplogo" alt="CalAI-Best-AI-Certification-Institute-in-USA" />
+        <img
+          src="techmahindra.webp"
+          alt="CalAI-Best-AI-Certification-Institute-in-USA"
+        />
+        <img
+          src="xerox.webp"
+          alt="CalAI-Best-AI-Certification-Institute-in-USA"
+        />
+        <img
+          src="codebright.webp"
+          alt="CalAI-Best-AI-Certification-Institute-in-USA"
+        />
+        <img
+          src="google.webp"
+          alt="CalAI-Best-AI-Certification-Institute-in-USA"
+        />
+        <img
+          src="intuit.webp"
+          alt="CalAI-Best-AI-Certification-Institute-in-USA"
+        />
+        <img
+          src="sap.webp"
+          alt="CalAI-Best-AI-Certification-Institute-in-USA"
+        />
+        <img
+          src="microsoft.webp"
+          alt="CalAI-Best-AI-Certification-Institute-in-USA"
+        />
+        <img src="hplogo" alt="CalAI-Best-AI-Certification-Institute-in-USA" />
+        <img
+          src="techmahindra.webp"
+          alt="CalAI-Best-AI-Certification-Institute-in-USA"
+        />
+        <img
+          src="xerox.webp"
+          alt="CalAI-Best-AI-Certification-Institute-in-USA"
+        />
+        <img
+          src="codebright.webp"
+          alt="CalAI-Best-AI-Certification-Institute-in-USA"
+        />
+        <img
+          src="google.webp"
+          alt="CalAI-Best-AI-Certification-Institute-in-USA"
+        />
+        <img
+          src="intuit.webp"
+          alt="CalAI-Best-AI-Certification-Institute-in-USA"
+        />
+        <img
+          src="sap.webp"
+          alt="CalAI-Best-AI-Certification-Institute-in-USA"
+        />
+        <img
+          src="microsoft.webp"
+          alt="CalAI-Best-AI-Certification-Institute-in-USA"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Slider;
