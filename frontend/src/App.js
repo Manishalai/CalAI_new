1c78c4ea36c79eda332bbab0b7c7dd9524ff1472
singhAman05 <amanshankarsingh05@gmail.com>
2024-06-03 18:01:23 +0530

    changes in pacjage.json file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
-import { Home } from "./Screens/Home.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Screens/Home.jsx";
+import AID from "./Screens/Know_more/AID.jsx";
+import AIM from "./Screens/Know_more/AIM.jsx";
+import AIL from "./Screens/Know_more/AIL.jsx";
+import PvP from "./Screens/TnC&PvP/PvPo.jsx";
+import TNC from "./Screens/TnC&PvP/TNC.jsx";
+import Blog_AI from "./Screens/Blogs/Blog6_AI.jsx";
+import Blog_ES from "./Screens/Blogs/Blog3_ES.jsx";
+import Blog_MP from "./Screens/Blogs/Blog5_MP.jsx";
+import Blog_UC from "./Screens/Blogs/Blog4_UC.jsx";
+import Blog_PF from "./Screens/Blogs/Blog2_PF.jsx";
+import Blog_ML from "./Screens/Blogs/Blog1_ML.jsx";
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/AI_Developer" element={<AID />} />
+        <Route path="/AI_Manager" element={<AIM />} />
+        <Route path="/AI_Leader" element={<AIL />} />
+        <Route path="/Privacy_Policy" element={<PvP />} />
+        <Route path="/Blog/Blog1" element={<Blog_AI />} />
+        <Route path="/Blog/Blog2" element={<Blog_ES />} />
+        <Route path="/Blog/Blog3" element={<Blog_ML />} />
+        <Route path="/Blog/Blog4" element={<Blog_MP />} />
+        <Route path="/Blog/Blog5" element={<Blog_PF />} />
+        <Route path="/Blog/Blog6" element={<Blog_UC />} />
+      </Routes>
+    </Router>
   );
 };
 
