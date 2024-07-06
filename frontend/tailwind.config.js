4095535e1ea0eced5033d9ba6adab28c7e77dc53
Manishalai <kristin.p@calai.org>
2024-07-06 11:41:57 +0530

    Add files via upload

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,34 +3,38 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
-      '3xl': {'min': '1920px'},
-      '2xl': {'max': '1535px'},
+      "3xl": { min: "1920px" },
+      "2xl": { max: "1535px" },
       // => @media (max-width: 1535px) { ... }
 
-      'xl': {'max': '1279px'},
+      xl: { max: "1279px" },
       // => @media (max-width: 1279px) { ... }
 
-      'lg': {'max': '1023px'},
+      lg: { max: "1023px" },
       // => @media (max-width: 1023px) { ... }
 
-      'md': {'max': '767px'},
+      md: { max: "767px" },
       // => @media (max-width: 767px) { ... }
 
-      'sm': {'max': '639px'},
+      sm: { max: "639px" },
       // => @media (max-width: 639px) { ... }
     },
     extend: {
       animation: {
-        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
+        shine: "shine 1s ease-in-out",
       },
       keyframes: {
-        'infinite-scroll': {
-          from: { transform: 'translateX(0)' },
-          to: { transform: 'translateX(-100%)' },
-        }
-      }         
-    
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+        shine: {
+          "0%": { backgroundPosition: "-200% center" },
+          "100%": { backgroundPosition: "200% center" },
+        },
+      },
+    },
   },
-},
   plugins: [],
 };
